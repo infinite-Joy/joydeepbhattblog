53de25b2eea35841b2a382ec0387d5146cd05467
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2016-10-20 23:49:20 +0530

    added the pelican conf

---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Joydeep Bhattacharjee'
+SITENAME = 'Joydeep Bhatt blog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# give the theme name
+THEME = "pelican_themes/pelican-themes/pelican-bootstrap3"
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('FlawCode podcast', 'https://www.flawcode.com'),)
+
+# Social widget
+SOCIAL = (('GitHub', 'https://github.com/infinite-Joy'),
+          ('Twitter', 'https://twitter.com/alt227Joydeep'),
+          ('LinkedIn',
+           'https://in.linkedin.com/in/joydeep-bhattacharjee-934a1157'),)
+
+DEFAULT_PAGINATION = 2
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
